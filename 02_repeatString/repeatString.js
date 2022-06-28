e739203108ae88c8cba3939d8ba4f16ba3c93e7f
a-mechler <amech73@gmail.com>
2022-06-28 11:17:56 -0500

    finished exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(String, Num) {
+    let reString = '';
+    if (Num < 0) return 'ERROR';
+    for (i = 0; i < Num; i++){
+        reString += String;
+    }
+    return reString;
 };
 
 // Do not edit below this line
